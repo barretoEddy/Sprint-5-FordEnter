b0ac45efeeb3146b52991a80b42a06ac612f81a3
Eduardo Barreto <barreto.eduardo@ufba.br>
2025-08-29 16:09:57 -0300

    refatorar o codigo do formulario

--- js/form.js
+++ js/form.js
@@ -2,34 +2,57 @@
 //class contato
 
 class contato {
-    constructor(nome, email, telefone, tipoContato, mensagem) {
+    constructor(nome, email, telefone, tipoContato, mensagem, aceitouTermos, aceitouNewsletter) {
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
         this.tipoContato = tipoContato;
         this.mensagem = mensagem;
+        this.aceitouTermos = aceitouTermos;
+        this.aceitouNewsletter = aceitouNewsletter;
     }
 }
 
 function Post(form, event) {
     // Prevenir o comportamento padrão do formulário (recarregar a página)
     event.preventDefault();
+
+    const termosCheckbox = document.getElementById("termosCheckbox");
+    const submitBtn = document.getElementById("submit-btn");
+
+    if (!termosCheckbox.checked) {
+        alert("Você deve aceitar os termos e condições para enviar o formulário.");
+        return false;
+    }
     
     let data = new contato(
         form.elements.namedItem("nome").value,
         form.elements.namedItem("email").value, 
         form.elements.namedItem("telefone").value, 
         form.elements.namedItem("contato").value,
-        form.elements.namedItem("mensagem").value
+        form.elements.namedItem("mensagem").value,
+        termosCheckbox.checked,
+        newsletterCheckbox.checked
     );
     
     console.log('Dados do formulário:', data);
     
     alert(`Obrigado sr(a) ${data.nome}, os seus dados foram encaminhados com sucesso!`);
+    termosCheckbox.checked = false;
+    newsletterCheckbox.checked = false;
+    submitBtn.disabled = true;
+
+    validarEnvio();
 
     form.reset();
 
     return false; 
+}
+
+function validarEnvio() {
+    const termosCheckbox = document.getElementById("termosCheckbox");
+    const submitButton = document.getElementById("submit-btn");
+    submitButton.disabled = !termosCheckbox.checked;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
